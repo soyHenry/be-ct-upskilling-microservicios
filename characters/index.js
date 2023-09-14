abcab3ca2dbc927a58af920cb73e91d320e3e58f
Ignacio Amatt <iamatt@soyhenry.com>
2023-09-14 14:42:24 -0300

    Revert "hw1"
    This reverts commit 43a261b1a9b10c5fe4a9f0ad81da679c9f92b156.

--- characters/index.js
+++
@@ -1,5 +0,0 @@
-const server = require ('./src/server.js')
-
-const PORT = 8001
-
-server.listen(PORT, () => console.log(`Characters service listening on port ${PORT}`)) 
--- characters/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express')
-const controllers = require('../controllers')
-const router = Router()
-
-router.get('/', controllers.getCharacters)
-
-module.exports = router
